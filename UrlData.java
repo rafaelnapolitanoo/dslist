828909cc3b22860d98162a3922889572ed4e5abc
Rafael Napolitano <161532393+rafaelnapolitanoo@users.noreply.github.com>
2024-12-03 17:53:43 -0300

    Add files via upload
    Redirect url shortener

---
+++ UrlData.java
@@ -0,0 +1,16 @@
+package com.rafaelnapolitano.createUrlShortner;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
